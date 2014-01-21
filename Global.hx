f25d2c4abcae93f6f485b06317cf8a2435038629
postite <blog@postite.com>
2014-01-21 13:21:11 +0100

    force selection to MSLAYER
    consider abstract ?

--- Global.hx
+++ Global.hx
@@ -9,14 +9,20 @@ public function new()
 		
         untyped __js__("Global.doc = doc");
         untyped __js__("Global.selection = selection");
+       
+        
 
     	
 }
 	
 	public static  var doc:MSDocument;
-	public static var selection:SketchArray<Dynamic>; //either MSLAyer or MSLAyerGroup ?
+	public static var selection:SketchArray<MSLayer>; //either MSLAyer or MSLAyerGroup ?
+	public static  var hxselection:String; //either MSLAyer or MSLAyerGroup ?
+
 	public static inline function log(m:String)
 	{
 		untyped log(m);
 	}
+
+	 
 }
