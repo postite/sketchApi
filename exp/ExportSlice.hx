7e4a36e9d5d6c828f402775b8a13883b7ab74284
postite <blog@postite.com>
2014-04-28 15:39:27 +0200

    new feature ( Slice & pseudo Slice)

--- exp/ExportSlice.hx
+++ exp/ExportSlice.hx
@@ -1,24 +1,85 @@
 package exp;
-import Global.*;
+using helpers.Layer;
+using helpers.Slice;
+using helpers.Document;
 import exp.ExportTypes;
+using helpers.StringSketch;
+import Global.*;
+
+
+
+
+
+
 class ExportSlice extends ExportLayer implements Exportable
 {
+
+public static var one:Bool=false;
 
 
 	public function new(layer:MSLayerGroup)
 	{
 		super(layer);
 		this.type=Slice;
+
 	}
 	override public function export():Exportable
-	{
-		_trace("export Slice");
-		 this.x=0;
-		 this.y=0;
-		 this.relx=0;
-		 this.rely=0;
-		this.width=0.1;
-		this.height=0;
+	{	
+		onelog(orig);
+		var slice:MSSliceLayer=null;
+		var layer:MSLayer=null;
+		switch(helpers.Layer._class(orig)){
+			case MSSliceLayer:
+				slice= cast orig;
+				layer=slice.parentGroup();
+			case MSLayerGroup:
+				slice=doc.sliceForRect(orig.frame());
+				layer=orig;
+			case MSLayer:
+				slice=doc.sliceForRect(orig.frame());
+				layer=orig;
+		}
+		var slice:MSSliceLayer= cast orig;
+		onelog(slice.absoluteRect().width() +"height="+slice.absoluteRect().height());
+		onelog(slice.absoluteRect().origin().x +"height="+slice.absoluteRect().origin().y);
+		onelog(slice.absoluteRect().rulerX() +"height="+slice.absoluteRect().rulerY());
+		onelog(slice.relativeRect().origin.x +"y="+slice.relativeRect().origin.y);
+		onelog("parent="+slice.ancestors()[slice.ancestors().length-2].name());
+		onelog("parent="+slice.parentGroup().name());
+		onelog("shouldTrim="+slice.exportOptions().shouldTrim());
+		onelog("includedLayerIds="+slice.exportOptions().includedLayerIds().length);
+		// for( l in slice.exportOptions().includedLayerIds().iterator()){
+		// 	onelog(l);
+		// }
+		onelog("layerOptions="+slice.exportOptions().layerOptions());
+		
+
+		var sizes=slice.exportOptions().sizes();
+				onelog("sizes="+slice.exportOptions().sizes());
+
+		for( a in sizes){
+			onelog(a.defaultName()); //@0.5
+			onelog(a.parentLayer().name());//sliceName
+			onelog(a.isVectorExport());//svg or not
+			onelog(a.name());//suffix
+			onelog(a.format());//png/svg...etc
+			onelog(a.visibleScaleType());//0
+			onelog(a.scale());//0.5
+		}
+		x=slice.absoluteRect().rulerX();
+		y=slice.absoluteRect().rulerY();
+		width=slice.frame().width();
+		height=slice.frame().height();
+		relx=slice.frame().x();
+		rely=slice.frame().y();
+
+
+		this.type=Image;
+		//this.format=Type.createEnum (Format,Std.String(slice.exportOptions().sizes().firstObject().format().capitalize()));
+		this.format=Std.string(slice.exportOptions().sizes().firstObject().format());
+		src=slice.export(layer,doc.dir()+"view/images",sizes.firstObject().scale());
+		src=relativeSrc(src);
+
 		return cast this;
 
 	}
