ee90a754ce8edf8c98e998205ed9858ffa2072f1
postite <blog@postite.com>
2014-04-28 15:40:07 +0200

    hiding stuff ( siblings)

--- helpers/Artboard.hx
+++ helpers/Artboard.hx
@@ -7,6 +7,7 @@ import exp.Behave;
 import haxe.EnumFlags;
 class Artboard{
 
+public static var one:Bool=false;
 private static var hiddenLayers:Map<Int,List<MSLayer>>= new Map();
   //private static var beaviours:Map<Int,EnumFlags<Behave>>=new Map();
 
@@ -51,7 +52,7 @@ private static var hiddenLayers:Map<Int,List<MSLayer>>= new Map();
   //cache les autres layers
 	public static function hideOtherLayers(a:MSArtboardGroup,currentLayer:MSLayer,withChilds:Bool=true)
 	{
-   
+   onelog( "------------hideOthers-----------for "+currentLayer.name());
 	 var parents:Array<MSLayer>=[];
    var node:MSLayer=currentLayer.parentGroup();
    while (node!=a){
@@ -59,18 +60,20 @@ private static var hiddenLayers:Map<Int,List<MSLayer>>= new Map();
         parents.push(node);
         node= node.parentGroup();
       }
+      
+
 		for ( layer in a.layers()){
 			if (currentLayer!=layer ){
           var layerParent=layer.parentGroup();
           //avec les enfants ( flat or Not)
           if(withChilds){
 				      if ( layer.isVisible()){
-                _trace("________visible________"+layerParent.name()+"currentLayer"+layer.name() +"visible="+layer.isVisible());
+               // _trace("________visible________"+layerParent.name()+"currentLayer"+layer.name() +"visible="+layer.isVisible());
                  try{ 
                   untyped(layer).setIsVisible(false);
                   setHiddenLayers(a,layer);
                  } catch(err:Dynamic){
-                  _trace("________________________"+ err);
+                  //_trace("________________________"+ err);
                 }
               
               }
@@ -79,14 +82,50 @@ private static var hiddenLayers:Map<Int,List<MSLayer>>= new Map();
           }
       }
 		}
+
+    hideSiblings(a,currentLayer,currentLayer,withChilds);
+
+
      for (p in parents){ 
+
       try p.setIsVisible(true) catch(err:Dynamic)_trace( err);
     //       // unsetHiddenLayers
         }
+
+        
+      
+
     parents=null;
 	}
+  private static function hideParentsSiblings(a:MSArtboardGroup,currentLayer:MSLayer,origLayer:MSLayer,withChilds:Bool):Void
+  {
+
+    var active:MSLayer= currentLayer.parentGroup();
+    while (active.parentGroup()!=a){
+
+      hideSiblings(a,active,origLayer,withChilds);
+      active=active.parentGroup();
+    }
+  }
+  private  static function hideSiblings(a:MSArtboardGroup,currentLayer:MSLayer,origLayer:MSLayer,withChilds:Bool){
+     
+      /// hide siblings but make it recursive
+     // _trace("1234"+ currentLayer.parentGroup().name());
+      for ( child in currentLayer.parentGroup().layers()){
+       // _trace("_1234 childname"+ child.name());
+            if( child!=currentLayer){
+           onelog("hide"+ child.name());
+            if(child.isVisible())
+            hideParentsSiblings(a,child,origLayer,withChilds);
+            setHiddenLayers(a,child);
+            untyped(child).setIsVisible(false);
+            }
+          }
+     
+  }
   
   private  static function showHiddenLayers(a:MSArtboardGroup){
+    onelog("--------------show---------------");
      for (hidden in getHiddenLayers(a)){
         hidden.setIsVisible(true);
      }
