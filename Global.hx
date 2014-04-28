23f5f22044cf052d72a3ca769ba1cb3e495e3a06
postite <blog@postite.com>
2014-04-28 15:36:06 +0200

    messing with trace fund (unique)

--- Global.hx
+++ Global.hx
@@ -15,15 +15,27 @@ public function new()
 
     	
 }
-	
+	static var tracable=["helpers.Artboard"];
 	public static  var doc:MSDocument;
 	public static var selection:SketchArray<MSLayer>; //either MSLAyer or MSLAyerGroup ?
 	public static  var hxselection:String; //either MSLAyer or MSLAyerGroup ?
 
-	public static inline function _trace(m:Dynamic,?p:haxe.PosInfos)
+	public static function onelog(m:Dynamic,?p:haxe.PosInfos):Void
 	{
+		
+		 if (cast (Type.resolveClass(p.className)).one){
+
+		_trace=function(m:Dynamic,?p:haxe.PosInfos){};
+		log(m);
+		untyped __js__("log")(p.className+ " "+p.lineNumber+"");
+		
+		}
+	}
+	dynamic public static  function _trace(m:Dynamic,?p:haxe.PosInfos)
+	{
+		
 		untyped __js__("log")(p.className+ " "+p.lineNumber+""+m);
-		//log(m);
+		
 	}
 
 	public static inline function log(m:Dynamic){
