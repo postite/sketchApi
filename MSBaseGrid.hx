cba531837f937f41804b7ea157e15455254a41c8
postite <blog@postite.com>
2014-04-29 18:11:27 +0200

    basegrid ... todo layoutGrid & simplegrid

---
+++ MSBaseGrid.hx
@@ -0,0 +1,39 @@
+extern class MSBaseGrid
+{
+	
+}
+
+
+/* sketch3
+
+@interface _MSBaseGrid : MSModelObject
+{
+}
+
+- (BOOL)isEqualForSync:(id)arg1;
+- (void)syncPropertiesMatchingReference:(id)arg1 withObject:(id)arg2;
+- (void)copyPropertiesToObjectCopy:(id)arg1;
+- (void)setAsParentOnChildren;
+- (void)decodePropertiesWithCoder:(id)arg1;
+- (void)encodePropertiesWithCoder:(id)arg1;
+- (void)fillInEmptyObjects;
+- (BOOL)hasDefaultValues;
+- (void)initEmptyObject;
+- (void)enumerateChildProperties:(CDUnknownBlockType)arg1;
+- (void)enumerateProperties:(CDUnknownBlockType)arg1;
+
+@end
+
+@interface MSBaseGrid : _MSBaseGrid
+{
+}
+
++ (void)initialize;
+- (id)horizontalGuidesForRulerData:(id)arg1 inRect:(struct CGRect)arg2;
+- (id)verticalGuidesForRulerData:(id)arg1 inRect:(struct CGRect)arg2;
+- (void)drawInRect:(struct CGRect)arg1 baseLine:(struct CGPoint)arg2 atZoom:(double)arg3;
+- (BOOL)shouldDraw;
+
+@end
+
+*/
