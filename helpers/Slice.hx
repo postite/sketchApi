e93961a3cc2302736261f132d421d94c59ff82b7
postite <blog@postite.com>
2014-04-29 18:12:38 +0200

    kick slice folder

--- helpers/Slice.hx
+++ helpers/Slice.hx
@@ -53,7 +53,7 @@ class Slice{
     	invisible=true;
     	layer.setIsVisible(true);
     }
-    path = path +"/"+page.name()+"/"+artboard.name()+"/slices/"+  layer.name().clean()+ '.'+Std.string(options.format).toLowerCase();
+    path = path +"/"+page.name()+"/"+artboard.name()+"/"+  layer.name().clean()+ '.'+Std.string(options.format).toLowerCase();
     _trace( path +"factor="+Std.string(factor));
    	
    	//var factor= (options.scale!=null)? options.scale :1;
