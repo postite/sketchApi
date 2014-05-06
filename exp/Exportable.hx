0638bdb29afcfda189ac39e515658abb10a3332a
postite <blog@postite.com>
2014-05-06 13:15:34 +0200

    added relx/y and w/h

--- exp/Exportable.hx
+++ exp/Exportable.hx
@@ -8,6 +8,10 @@ interface Exportable{
 	public var name:String;
 	public var x(default,null):Float;
 	public var y(default,null):Float;
+	public var relx(default,null):Float;
+	public var rely(default,null):Float;
+	public var height(default,null):Float;
+	public var width(default,null):Float;
 	public var src:String;
 	public var type:exp.ExportTypes;
 	public function toObj():Dynamic;
