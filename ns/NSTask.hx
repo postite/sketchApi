ac236fcc17903f0d3fdcd760e5d7d576060bdfd7
postite <blog@postite.com>
2014-05-12 17:34:52 +0200

    added nsTask

--- ns/NSTask.hx
+++ ns/NSTask.hx
@@ -1,5 +1,15 @@
 package ns;
+@:native("NSTask")
 extern class NSTask{
 	//don't get that stuff
-	static public function alloc():Void;
+	static public function alloc():NSTask;
+	public function init():ns.NSTask;
+	public function setCurrentDirectoryPath(path:String):Void;
+	public function setLaunchPath(s:String):Void;
+	public function launch():Void;
+	 public function setArguments(args:ns.NSArray):Void;
+
+	
+	
+	
 }
