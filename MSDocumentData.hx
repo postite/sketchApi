bb2e47a67c1ed2585abeae7582c73f4d17d66bda
postite <blog@postite.com>
2014-09-02 15:41:28 +0200

    new Kid OTHB

--- MSDocumentData.hx
+++ MSDocumentData.hx
@@ -10,41 +10,53 @@ extern class MSDocumentData
 
 /*
 
+#import "MSModelObject.h"
+
+@class MSArray, MSImageCollection, MSSharedLayerContainer, MSSharedLayerStyleContainer, MSSharedLayerTextStyleContainer;
+
 @interface _MSDocumentData : MSModelObject
 {
     unsigned long long _currentPageIndex;
-    MSLayerStylesController *_layerStyles;
+    MSImageCollection *_images;
+    MSSharedLayerStyleContainer *_layerStyles;
+    MSSharedLayerContainer *_layerSymbols;
+    MSSharedLayerTextStyleContainer *_layerTextStyles;
     MSArray *_pages;
-    MSTextStyleController *_textStyles;
 }
 
-@property(retain, nonatomic) MSTextStyleController *textStyles; // @synthesize textStyles=_textStyles;
 @property(retain, nonatomic) MSArray *pages; // @synthesize pages=_pages;
-@property(retain, nonatomic) MSLayerStylesController *layerStyles; // @synthesize layerStyles=_layerStyles;
+@property(retain, nonatomic) MSSharedLayerTextStyleContainer *layerTextStyles; // @synthesize layerTextStyles=_layerTextStyles;
+@property(retain, nonatomic) MSSharedLayerContainer *layerSymbols; // @synthesize layerSymbols=_layerSymbols;
+@property(retain, nonatomic) MSSharedLayerStyleContainer *layerStyles; // @synthesize layerStyles=_layerStyles;
+@property(retain, nonatomic) MSImageCollection *images; // @synthesize images=_images;
 @property(nonatomic) unsigned long long currentPageIndex; // @synthesize currentPageIndex=_currentPageIndex;
 - (void).cxx_destruct;
+- (BOOL)isEqualForSync:(id)arg1;
+- (void)syncPropertiesMatchingReference:(id)arg1 withObject:(id)arg2;
 - (void)copyPropertiesToObjectCopy:(id)arg1;
-- (void)setUndoManagerOnChildren:(id)arg1;
 - (void)setAsParentOnChildren;
-- (void)decodePropertiesCompatibleWithCoder:(id)arg1;
-- (void)decodePropertiesManuallyWithCoder:(id)arg1;
 - (void)decodePropertiesWithCoder:(id)arg1;
-- (void)encodePropertiesCompatibleWithCoder:(id)arg1;
-- (void)encodePropertiesManuallyWithCoder:(id)arg1;
 - (void)encodePropertiesWithCoder:(id)arg1;
 - (void)fillInEmptyObjects;
 - (BOOL)hasDefaultValues;
 - (void)initEmptyObject;
-- (void)setPrimitiveTextStyles:(id)arg1;
-- (id)primitiveTextStyles;
 - (void)setPrimitivePages:(id)arg1;
 - (id)primitivePages;
+- (void)setPrimitiveLayerTextStyles:(id)arg1;
+- (id)primitiveLayerTextStyles;
+- (void)setPrimitiveLayerSymbols:(id)arg1;
+- (id)primitiveLayerSymbols;
 - (void)setPrimitiveLayerStyles:(id)arg1;
 - (id)primitiveLayerStyles;
+- (void)setPrimitiveImages:(id)arg1;
+- (id)primitiveImages;
 - (void)setPrimitiveCurrentPageIndex:(unsigned long long)arg1;
 - (unsigned long long)primitiveCurrentPageIndex;
+- (void)enumerateChildProperties:(CDUnknownBlockType)arg1;
 - (void)enumerateProperties:(CDUnknownBlockType)arg1;
 
 @end
+
+
 
 */
