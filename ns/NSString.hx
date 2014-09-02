e4f607148fba8dc1775e4316d171e8b396ebc1a0
postite <blog@postite.com>
2014-09-02 15:42:48 +0200

    utf8 by default

--- ns/NSString.hx
+++ ns/NSString.hx
@@ -3,7 +3,7 @@ package ns;
 extern class NSString{
 
 	public static function stringWithString(s:String):NSString;
-	inline public function writeToFile(path:String,atomically:Bool):Bool{
-		return untyped this.writeToFile_atomically(path,atomically);
+	inline public function writeToFile(path:String,atomically:Bool,encoding:Dynamic,error:Dynamic):Bool{
+		return untyped this.writeToFile_atomically_encoding_error(path,atomically,encoding,error);
 	}
 }
