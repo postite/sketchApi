45397a038d79b46c5eb30c7e2c93a38f586389e7
postite <blog@postite.com>
2014-09-04 17:03:15 +0200

    new ext

---
+++ MSTextStyle.hx
@@ -0,0 +1,20 @@
+extern class MSTextStyle{
+
+
+}
+
+
+/*
+#import "_MSTextStyle.h"
+
+@interface MSTextStyle : _MSTextStyle
+{
+}
+
++ (id)styleWithAttributes:(id)arg1;
+- (void)syncOwningTextLayerWithThisStyle;
+- (void)setAttributes:(id)arg1;
+- (id)treeAsDictionary;
+
+@end
+*/
