a935d961fbed61275ba837546342e08a2f49cf14
postite <blog@postite.com>
2014-09-04 17:01:22 +0200

    new Feature export Styled text

---
+++ exp/ExportStyledText.hx
@@ -0,0 +1,66 @@
+package exp;
+import Global.*;
+import exp.ExportText.TextProperties;
+
+typedef StyleId=String;
+typedef StyleName=String;
+class ExportStyledText extends ExportText{
+
+	static var styles:Map<StyleId,StyleName>;
+	static var css:Map<StyleName,TextProperties>;
+	public function new (origLayer:MSLayer):Void
+	{
+		init();
+		super( cast origLayer);
+
+		
+	}
+	function  init():Void
+	{
+		if(styles==null){
+			styles=new Map();
+			css=new Map();
+			var datastyles=doc.documentData().layerTextStyles().objects();
+			for ( style in datastyles){
+				styles.set(style.style().sharedObjectID(),style.name());
+
+			}
+			log( styles);
+		}
+	}
+	public function styleName():String
+	{
+		return (getStyleName(this.orig.style().sharedObjectID()));
+	}
+	public function getStyleName(id:StyleId):String
+	{
+		return styles.get(id);
+	}
+
+	override function export():Exportable
+	{
+		super.export();
+		css.set(this.styleName(),this.TP);
+		this.type=StyledText;
+		return this;
+	}
+	public static function generateCss():String
+	{
+		var cssString="";
+		for (style in css.keys()){
+			_trace( "key="+style);
+			var texteProps=css.get(style);
+		//var lineHeight=(this.height>= texteProps.lineSpacing*1.5)? texteProps.lineSpacing : node.height;
+		var css='font-size:${texteProps.fontSize}px;\n';
+		css+='font-family:"${texteProps.fontPostscriptName}";\n';
+		css+='text-align:${texteProps.textAlignment};\n';
+		css+='line-height:${texteProps.lineSpacing}px;\n';
+		css+='color:#${texteProps.color};\n';
+								
+		cssString+='.$style{\n$css\n}\n';
+			
+		}
+		return cssString;
+
+	}
+}
