9878a9f956a26e30877567b484ba5bfaba765553
postite <blog@postite.com>
2014-09-06 14:53:05 +0200

    clean up

--- MSSharedLAyerContainer.hx
+++ MSSharedLAyerContainer.hx
@@ -1,12 +1,57 @@
 
 
 extern class MSSharedLayerContainer{
+		
+	public function symbolForInstance(id:Dynamic):Dynamic;
 
 	//not documented
-	public function objects():SketchArray<MSSharedLayerStyle>;
+	public function objects():SketchArray<MSSharedLayer>;
 	
 }
 
+extern class MSSharedLayerTextStyleContainer {
+public function objects():SketchArray<MSSharedLayerStyle>;
+	
+}
+extern class  MSSharedLayer{
+	//undoc 
+	public function instances():SketchArray<String>; // MSLAyer.originalObjectID
+	
+	public function name():String;
+	public function symbol():MSLayerGroup;
+	public function defaultName():String; //Symbol
+	public function type():Int; //0
+	
+	
+}
+
+extern class MSSharedLayerStyle{
+
+	//undoc
+	public function name():String;
+	public function defaultName():String;
+	public function type():Int;
+	public function style():MSStyle;
+	public function newInstance():Dynamic;
+
+	
+	
+}
+/*
+@interface MSSharedLayer : _MSSharedLayer
+{
+}
+
+- (id)newInstanceReplacingLayer:(id)arg1;
+- (void)prepareToBecomeSharedObjectValue:(struct MSModelObject *)arg1;
+- (unsigned long long)type;
+- (id)newUnregisteredInstance;
+- (id)newInstance;
+- (id)symbol;
+- (id)defaultName;
+
+@end
+*/
 
 
 /*
@@ -36,4 +81,23 @@ extern class MSSharedLayerContainer{
 
 @end
 
+*/
+
+/*
+
+#import "_MSSharedLayerStyle.h"
+
+@class MSStyle;
+
+@interface MSSharedLayerStyle : _MSSharedLayerStyle
+{
+}
+
+- (unsigned long long)type;
+- (id)newInstance;
+@property(readonly, nonatomic) MSStyle *style;
+- (id)defaultName;
+- (void)migratePropertiesFromV23OrEarlierWithCoder:(id)arg1;
+
+@end
 */
