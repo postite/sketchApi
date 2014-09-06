784619f0cd3ce04bc2f22b69d90c9c0d84c8f33d
postite <blog@postite.com>
2014-09-06 14:51:51 +0200

    init

--- exp/ExportSymbol.hx
+++ exp/ExportSymbol.hx
@@ -6,22 +6,47 @@ import exp.ExportTypes;
 using helpers.StringSketch;
 import Global.*;
 
-
-
-
-
-
+typedef OriginalId=String;
+typedef SymbolName=String;
 class ExportSymbol extends ExportLayer implements Exportable
 {
-
-public static var one:Bool=false;
-
-
+	static var symbols:Map<OriginalId,SymbolName>;
+	//static var css:Map<StyleName,TextProperties>;
 	public function new(layer:MSLayerGroup)
 	{
+		init();
 		super(layer);
 		this.type=Symbol;
 
+	}
+
+	function  init():Void
+	{
+		if(symbols==null){
+			symbols=new Map();
+			//css=new Map();
+			var symbolsData=doc.documentData().layerSymbols().objects();
+			for ( symData in symbolsData){
+				for (i in  (symData.instances():SketchArray<Dynamic>))
+				symbols.set(symData.symbol().originalObjectID(),symData.name());
+			
+			}
+			
+		}
+	}
+	public function symbolName():String
+	{
+		return (getStyleName(this.orig.originalObjectID()));
+	}
+	public function getStyleName(id:OriginalId):String
+	{
+		return symbols.get(id);
+	}
+
+	override function export():Exportable
+	{
+		super.export();
+		return this;
 	}
 	
 	
